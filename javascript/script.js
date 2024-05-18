c7530ac6a541c379d57d51d5a4599354201013fd
RoxanLagto <rpl1771@students.uc-bcf.edu.ph>
2024-05-18 22:00:54 +0800

    Personal Portfolio

---
+++ javascript/script.js
@@ -0,0 +1,17 @@
+function showSection(section) {
+    switch(section) {
+        case 'about':
+            window.location.href = 'path/to/about-page.html'; // Replace with the actual URL
+            break;
+        case 'myportfolio':
+            window.location.href = 'path/to/portfolio-page.html'; // Replace with the actual URL
+            break;
+        case 'getintouch':
+            window.location.href = 'path/to/contact-page.html'; // Replace with the actual URL
+            break;
+        default:
+            // Default action, if needed
+            break;
+    }
+}
+
